fc87eebf58728796676b7ed9f6561d3debeb09a2
Pmelinte <pmelinte@gmail.com>
2025-08-05 22:01:56 +0300

    Fix: export implicit DB pentru compatibilitate Vercel

--- lib/db.ts
+++ lib/db.ts
@@ -4,6 +4,8 @@ import { open } from 'sqlite';
 export async function getDBConnection() {
   return open({
     filename: './swaply.db',
-    driver: sqlite3.Database
+    driver: sqlite3.Database,
   });
 }
+
+export default getDBConnection;
